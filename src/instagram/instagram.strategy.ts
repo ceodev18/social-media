ed769b4f8e808fb9db6dea1455e5e0f08520dd45
Christian Espinoza <cespinoza@foresttravel.com>
2023-12-17 23:54:34 -0500

    callback updated

--- src/instagram/instagram.strategy.ts
+++ src/instagram/instagram.strategy.ts
@@ -12,6 +12,8 @@ export class InstagramStrategy extends PassportStrategy(Strategy, 'instagram') {
       clientID: process.env.INSTAGRAM_CLIENT_ID,
       clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
       callbackURL: process.env.INSTAGRAM_CALLBACK_URL,
+      scope: 'user_profile,user_media',
+      responseType: 'code'
     });
   }
   async validate(
